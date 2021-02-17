b4df60291962ba6ff510c92b1fd53e40039ba356
Alex Ines <alex.ines@pm.me>
2021-02-17 16:36:26 +0100

    Link events from event list to detail page of each event

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'events' => 'events#index'
-  get 'events/:id' => 'events#show'
+  get 'events/:id' => 'events#show', as: 'event'
 
 end
