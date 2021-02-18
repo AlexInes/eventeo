faaf08525902ed1a1f1046f6e9fe4eed2e2aa179
Alex Ines <alex.ines@pm.me>
2021-02-18 13:02:23 +0100

    Add feature that allows editing existing events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,16 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    event_params = params.require(:event).
+                   permit(:name, :description, :location, :price, :event_start_date, :event_end_date)
+    @event.update(event_params)
+    redirect_to @event
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'events/index'
+
+  root 'events#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'events' => 'events#index'
   get 'events/:id' => 'events#show', as: 'event'
-
+  get 'events/:id/edit' => 'events#edit', as: 'edit_event'
+  patch 'events/:id' => 'events#update'
 end
